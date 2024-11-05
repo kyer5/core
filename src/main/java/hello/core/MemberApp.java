467ca035a6edf9bf8bfdda400094ef2f1513e0c8
강예린 <yerin7786@gmail.com>
2024-11-05 22:05:42 +0900

    section4. 스프링으로 전환하기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // -> 스프링 컨테이너, AppConfig에 있는 설정 정보를 가지고 스프링 컨테이너에 객체 생성한 것들을 다 넣어준다.
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);// 스프링 컨테이너에서 꺼낼 객체의 이름 지정
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
