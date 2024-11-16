1c36bf6d4336c419e58df11a67b78675614fb77f
강예린 <yerin7786@gmail.com>
2024-11-16 21:10:39 +0900

    section8. @Autowired 필드 명, @Qualifier, @Primary

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,17 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final이 붙은 값을 파라미터로 받는 생성자 생성
+//@RequiredArgsConstructor // final이 붙은 값을 파라미터로 받는 생성자 생성
 public class OrderServiceImpl implements OrderService {
 
     // 필드 주입 (안티 패턴 -> 외부에서 변경 불가능해서 테스트 어려움)
@@ -26,11 +28,11 @@ public class OrderServiceImpl implements OrderService {
         this.discountPolicy = discountPolicy;
     }*/
 
-    /*@Autowired*//* // 생성자 주입, 생성자가 딱 1개만 있으면 @Autowired를 생략해도 자동 주입된다. -> @RequiredArgsConstructor가 모든 걸 해결
+    @Autowired // 생성자 주입, 생성자가 딱 1개만 있으면 @Autowired를 생략해도 자동 주입된다. -> @RequiredArgsConstructor가 모든 걸 해결
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
-    }*/
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
